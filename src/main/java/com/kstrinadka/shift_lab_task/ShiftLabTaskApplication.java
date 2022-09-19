131745dfe13436770eaf111a10c2fb3c4324b41c
kstrinadka <k.strinadka@gmail.com>
2022-09-19 14:56:04 +0700

    first commit

---
+++ src/main/java/com/kstrinadka/shift_lab_task/ShiftLabTaskApplication.java
@@ -0,0 +1,13 @@
+package com.kstrinadka.shift_lab_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftLabTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiftLabTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kstrinadka/shift_lab_task/ShiftLabTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kstrinadka.shift_lab_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftLabTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
